607a20d618d58a6c49aea2388e36f4e534d0ca19
Nick Gauthier <ngauthier@email.arizona.edu>
2020-03-20 20:15:13 -0700

    simpler area weighting

--- R/calc_pcs.R
+++ R/calc_pcs.R
@@ -10,19 +10,11 @@
 #' @examples
 calc_pcs <- function(dat, scale = TRUE){
   dat %>%
+    dplyr::mutate(SWE = SWE * cos(y * pi / 180)) %>% # area weight
     tidyr::spread(year, SWE) %>%
     dplyr::select(-x, -y) %>%
     t() %>%
     prcomp(scale. = scale)
 }
 
-area_weight <- function(x){
-  names_x <- names(x)
-  x %>%
-    init('y') %>% # get a map of latitudes
-    `*`(pi/180) %>% # convert to radians
-    cos %>% # cosine
-    sqrt %>%
-    `*`(x) %>%
-    `names<-`(names_x)
-}
+
