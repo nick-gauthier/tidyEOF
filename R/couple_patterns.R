e47342d6676b9ba570b9ff6b63b4e497eebadeaa
Nick Gauthier <ngauthier@email.arizona.edu>
2020-04-14 18:06:15 -0700

    modularize pattern coupling function

--- R/couple_patterns.R
+++ R/couple_patterns.R
@@ -8,22 +8,48 @@
 #'
 #' @examples
 couple_patterns <- function(patterns.x, patterns.y) {
-  amps <- if('patterns' %in% class(patterns.x)) patterns.x$amplitudes else patterns.x
-  predictors <- amps %>%
+
+  data_in <- prep_data(patterns.x, patterns.y)
+
+  model <- fit_model(data_in)
+
+  data_out <- model %>%
+    dplyr::select(PC, data) %>%
+    unnest(cols = c(data)) %>%
+    dplyr::select(PC, amplitude, year, pred, resid)
+
+  # would it be worth it to also return the reconstructed spatial field here or not?
+  coupled_patterns <- list(model = dplyr::select(model, -data),
+                           data = data_out)
+  class(coupled_patterns) <- 'coupled_patterns'
+  return(coupled_patterns)
+}
+
+prep_data <- function(patterns.x, patterns.y) {
+  amps.x <- if('patterns' %in% class(patterns.x)) patterns.x$amplitudes else patterns.x
+  amps.y <- if('patterns' %in% class(patterns.y)) patterns.y$amplitudes else patterns.y
+
+    predictors <- amps.x %>%
     mutate(PC = paste0('PC', PC)) %>%
     spread(PC, amplitude)
 
-  predictands <- patterns.y$amplitudes %>%
-    mutate(PC = paste0('PC', PC))
+  predictands <- amps.y %>%
+    mutate(PC = paste0('PC', PC)) %>%
+    dplyr::select(year, PC, amplitude)
 
-  gam_formula <- names(predictors) %>%
-    map( ~ paste0('s(', ., ', k = 4)')) %>%
+  inner_join(predictands, predictors, by = 'year')
+}
+
+fit_model <- function(data_in) {
+  gam_formula <- data_in %>%
+    dplyr::select(-PC, -amplitude) %>% #, -year) %>%
+    names() %>%
+    map( ~ paste0("s(", ., ", bs = 'cr', k = 3)")) %>%
     paste(collapse = ' + ') %>%
     paste('amplitude ~ ', .) %>%
     as.formula()
 
-  # should check how many overlapping years there are during model fitting, print result, and error if none
-  model <- inner_join(predictands, predictors, by = 'year') %>%
+  model <- data_in %>%
     group_by(PC) %>%
     nest() %>%
     mutate(mod = map(data, ~ gam(
@@ -38,15 +64,4 @@ couple_patterns <- function(patterns.x, patterns.y) {
       data = map2(data, mod, add_predictions),
       data = map2(data, mod, add_residuals)
     )
-
-  data <- model %>%
-    dplyr::select(PC, data) %>%
-    unnest(cols = c(data)) %>%
-    dplyr::select(PC, amplitude, year, pred, resid)
-
-  # would it be worth it to also return the reconstructed spatial field here or not?
-  coupled_patterns <- list(model = dplyr::select(model, -data),
-                           data = data)
-  class(coupled_patterns) <- 'coupled_patterns'
-  return(coupled_patterns)
 }
