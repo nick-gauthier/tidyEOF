9393fc051827f10d88459b9cad91be54224721f5
Nick Gauthier <ngauthier@email.arizona.edu>
2020-04-14 18:06:39 -0700

    more flexible predict

--- R/predict_patterns.R
+++ R/predict_patterns.R
@@ -7,14 +7,15 @@
 #' @export
 #'
 #' @examples
-predict_patterns <- function(coupled_patterns, new_amps) {
+predict_patterns <- function(coupled_patterns, predictors) {# new_amps) {
 
-  amps <- if('patterns' %in% class(new_amps)) new_amps$amplitudes else new_amps
-  predictors <- amps %>%
-    mutate(PC = paste0('PC', PC)) %>%
-    spread(PC, amplitude)
+ # amps <- if('patterns' %in% class(new_amps)) new_amps$amplitudes else new_amps
+  #predictors <- amps %>%
+   # mutate(PC = paste0('PC', PC)) %>%
+    #spread(PC, amplitude)
 
-  coupled_patterns$model$mod %>%
+  models <- if('coupled_patterns' %in% class(coupled_patterns)) coupled_patterns$model else coupled_patterns
+  models$mod %>%
     map(~add_predictions(predictors, ., var = 'amplitude', type = 'response')) %>%
     bind_rows(.id = 'PC') %>%
     dplyr::select(year, PC, amplitude)
@@ -27,11 +28,12 @@ reconstruct_field <- function(target_patterns, amplitudes = NULL) {
   left_join(amplitudes, eofs, by = c('PC' = 'EOF')) %>%
     mutate(PC = as.numeric(PC)) %>%
     mutate(anomaly = weight * amplitude) %>%
-    dplyr::select(-c(amplitude, value, weight)) %>%
+    dplyr::select(-c(amplitude, weight)) %>%
     group_by(x, y, year) %>%
     summarize(anomaly = sum(anomaly)) %>%
     left_join(target_patterns$climatology, by = c("x", "y")) %>%
     mutate(SWE = anomaly + swe_mean,
            SWE = if_else(SWE < 0, 0, SWE))%>%
-    dplyr::select(-c(swe_mean, swe_sd))
+    dplyr::select(-c(swe_mean, swe_sd)) %>%
+    ungroup()
 }
