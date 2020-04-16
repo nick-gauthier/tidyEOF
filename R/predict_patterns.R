ba355f80c98e481dea1f3de72de638bac6cb47c8
Nick Gauthier <ngauthier@email.arizona.edu>
2020-04-15 21:04:02 -0700

    threshold for calculating SCA
    also forecast correlation plot

--- R/predict_patterns.R
+++ R/predict_patterns.R
@@ -9,14 +9,17 @@
 #' @examples
 predict_patterns <- function(coupled_patterns, predictors) {# new_amps) {
 
- # amps <- if('patterns' %in% class(new_amps)) new_amps$amplitudes else new_amps
-  #predictors <- amps %>%
-   # mutate(PC = paste0('PC', PC)) %>%
-    #spread(PC, amplitude)
+  preds <- if('patterns' %in% class(predictors)) predictors$amplitudes else new_amps
+if('patterns' %in% class(predictors)) {
+  preds <- preds %>%
+   mutate(PC = paste0('PC', PC)) %>%
+  spread(PC, amplitude)
+}
+
 
   models <- if('coupled_patterns' %in% class(coupled_patterns)) coupled_patterns$model else coupled_patterns
   models$mod %>%
-    map(~add_predictions(predictors, ., var = 'amplitude', type = 'response')) %>%
+    map(~add_predictions(preds, ., var = 'amplitude', type = 'response')) %>%
     bind_rows(.id = 'PC') %>%
     dplyr::select(year, PC, amplitude)
 }
