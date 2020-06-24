f0bd0c9c2be362b20f6772b7c68d7713b2aa6d2c
Nick Gauthier <ngauthier@email.arizona.edu>
2020-06-23 17:27:24 -0700

    more flexible 3 parameter cv

--- R/validate.R
+++ R/validate.R
@@ -1,17 +1,18 @@
-prep_kfold <- function(k_preds, k_obs, preds, obs, type = NULL){
-  # this should be refactored ...
+prep_folds <- function(years, kfolds = 5){
+  # divide years into 5 contiguous folds
+  n <- length(years)
+  r <- n %% kfolds
+  fold_times <- rep(n %/% kfolds, kfolds)
+  fold_times[1:r] <- fold_times[1:r] + 1
+  tibble(year = years, fold = rep(1:kfolds, times = fold_times))
+}
 
+prep_cca <- function(preds, obs, k_preds, k_obs) {
   # find the years of overlap between the response and predictor fields
   years <- intersect(preds$year, obs$year)
   preds <- filter(preds, year %in% years)
   obs <- filter(obs, year %in% years)
-
-  # divide years into 5 contiguous folds
-  n <- length(years)
-  r <- n %% 5
-  fold_times <- rep(n %/% 5, 5)
-  fold_times[1:r] <- fold_times[1:r] + 1
-  folds <- tibble(year = years, fold = rep(1:5, times = fold_times))
+  folds <- prep_folds(years)
 
   # preprocess the training data for each fold
   train_obs <- map(1:5, ~ filter(folds, fold == .) %>%
@@ -29,14 +30,73 @@ prep_kfold <- function(k_preds, k_obs, preds, obs, type = NULL){
   tibble(train_obs, train_preds, test)
 }
 
-fit_cv <- function(dat, fun) {
-  pmap_dfr(list(dat$train_preds, dat$train_obs, dat$test), fun) %>%
-    arrange(x, y, year) %>%
-    get_errors()%>%
-    group_by(x,y) %>%
-    filter(median(SWE_obs) > 3) %>%
-    get_scores()
+
+prep_eot <- function(preds, obs, k_preds, k_obs, k_cca){
+  # find the years of overlap between the response and predictor fields
+  years <- intersect(preds$year, obs$year)
+  preds <- filter(preds, year %in% years)
+  obs <- filter(obs, year %in% years)
+  folds <- prep_folds(years)
+
+  # preprocess the training data for each fold
+  obs_train_rast <- purrr::map(1:5, ~ filter(folds, fold == .) %>%
+                                 anti_join(obs, ., by = 'year') %>% # training years
+                                 spread(year, SWE) %>%
+                                 rasterFromXYZ())
+
+  pred_train_rast <- purrr::map(1:5, ~ filter(folds, fold == .) %>%
+                                  anti_join(preds, ., by = 'year') %>% # training years
+                                  spread(year, SWE) %>%
+                                  rasterFromXYZ())
+
+  obs_train <- purrr::map(obs_train_rast, ~ anomalize(.) %>%
+                            denoise(k = k_obs, weighted = TRUE, verbose = FALSE))
+
+  pred_train <-  purrr::map(pred_train_rast, ~ anomalize(.) %>%
+                             denoise(k = k_preds, weighted = TRUE, verbose = FALSE))
+
+  # preprocess test data for each fold
+  test <- purrr::map(1:5, ~ filter(folds, fold == .) %>%
+                       semi_join(preds, ., by = 'year') %>%
+                       spread(year, SWE) %>%
+                       rasterFromXYZ())%>% # test years
+    map2(pred_train_rast, ~.x - mean(.y)) # subtract the training predictor mean from the test predictors
+
+  # fit model to training data and use to predict new fields
+  list(eot = map2(pred_train, obs_train, ~eot(.x, .y, n = k_cca, standardised = FALSE, verbose = FALSE)), # this hard codes 10 patterns, but could be changed to min(k_preds, k_obs)
+       test = test,
+       mean = purrr::map(obs_train_rast, mean))
 }
+
+predict_eot <- function(cv, k) {
+  pmap(list(cv$eot, cv$test, cv$mean), ~predict(..1, ..2, n = k) + ..3) %>%
+    brick
+}
+
+
+fit_cv <- function(dat, fun, k) {
+  errors <- pmap_dfr(list(dat$train_preds, dat$train_obs, dat$test), fun, k = k) %>%
+    arrange(x, y, year) %>%
+    get_errors()
+
+  c(rmse = summarise(errors, rmse = sqrt(mean(error^2))) %>% pull(rmse),
+    corr = total_swe_corr(errors))
+}
+
+
+total_swe_corr <- function(errors) {
+  errors %>%
+    left_join(areas, by = c("x", "y")) %>%
+    group_by(year) %>%
+    summarise(SWE_obs = sum(SWE_obs * area),
+              SWE_recon = sum(SWE_recon * area)) %>%
+    summarise(correlation = cor(SWE_recon, SWE_obs)) %>%
+    pull(correlation)
+}
+
+
+
+
 
 
 # special predict method for cv that scales predictors (should fold into original predict method in future)
@@ -66,7 +126,9 @@ predict_gam <- function(preds, obs, newdata) {
     reconstruct_field(obs, .)  # use train_obs to get training pcs
 }
 
-predict_cca <- function(preds, obs, newdata) {
+
+
+predict_cca <- function(preds, obs, newdata, k) {
   obs_amps <- obs$amplitudes %>%
                           mutate(PC = as.numeric(PC)) %>% # so spread doesn't lose order of columns
                           spread(PC, amplitude) %>%
@@ -95,13 +157,13 @@ predict_cca <- function(preds, obs, newdata) {
     scale(center = FALSE, scale = preds$pca$sdev) %>% # this should be from the training pcs
     .[,1:k_preds, drop = FALSE]
 
-  k <- length(cca$cor)
+  # add a check here if k <= min(k_preds, k_obs)
 
   new_years <- unique(newdata$year)
 
   new_amps %*%
     cca$xcoef[,1:k, drop = FALSE] %*% # drop = FALSE prevents a vector returning when k = 1
-    diag(cca$cor, nrow = k) %*%  # nrow likewise rpevents weired behavior when k = 1
+    diag(cca$cor, nrow = k) %*%  # nrow likewise prevents weird behavior when k = 1
     solve(cca$ycoef)[1:k,,drop = FALSE] %>%
     `rownames<-`(new_years) %>%
     as_tibble(rownames = 'year') %>%
@@ -117,10 +179,10 @@ predict_cca <- function(preds, obs, newdata) {
 get_errors <- function(x) {
   x %>%
     inner_join(prism_dat, by = c('x', 'y', 'year'), suffix = c('_recon', '_obs')) %>%
-    mutate(error = SWE_recon - SWE_obs,
-           relative_error = error / SWE_obs,
-           accuracy_ratio = if_else(SWE_recon < 0.03, 0.03, SWE_recon) /  if_else(SWE_obs < 0.03, 0.03, SWE_obs),
-           log_q = log(accuracy_ratio))
+    mutate(error = SWE_recon - SWE_obs)
+        #   relative_error = error / SWE_obs,
+         #  accuracy_ratio = if_else(SWE_recon < 0.03, 0.03, SWE_recon) /  if_else(SWE_obs < 0.03, 0.03, SWE_obs),
+        #   log_q = log(accuracy_ratio))
 }
 
 get_scores <- function(x) {
