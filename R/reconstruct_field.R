9e36400310ee00e0160fc3108421a180ae334a4c
Nick Gauthier <ngauthier@email.arizona.edu>
2021-05-10 12:15:17 -0700

    reconstruct_field works with stars objects

--- R/reconstruct_field.R
+++ R/reconstruct_field.R
@@ -9,17 +9,24 @@
 #' @examples
 reconstruct_field <- function(target_patterns, amplitudes = NULL) {
   if(is.null(amplitudes)) amplitudes <- target_patterns$amplitudes
-  eofs <- target_patterns$eofs
+  eofs <- as_tibble(target_patterns$eofs)
+  clim <- as_tibble(target_patterns$climatology)
 
-  left_join(amplitudes, eofs, by = c('PC' = 'EOF')) %>%
-    mutate(PC = as.numeric(PC)) %>%
+  amplitudes %>%
+    pivot_longer(-time, names_to = 'PC', values_to = 'amplitude') %>%
+    left_join(eofs, by = 'PC') %>%
     mutate(anomaly = weight * amplitude) %>%
     dplyr::select(-c(amplitude, weight)) %>%
-    group_by(x, y, year) %>%
-    summarize(anomaly = sum(anomaly)) %>%
-    left_join(target_patterns$climatology, by = c("x", "y")) %>%
-    mutate(SWE = anomaly + swe_mean,
-           SWE = if_else(SWE < 0, 0, SWE))%>% # can't have negative swe
-    dplyr::select(-c(swe_mean, swe_sd, anomaly)) %>%
-    ungroup()
+    group_by(x, y, time) %>%
+    summarize(anomaly = sum(anomaly, na.rm = TRUE), .groups = 'drop') %>%
+    left_join(clim, by = c("x", "y")) %>%
+    # should rewrite so SWE isn't hard coded
+    mutate(SWE = anomaly + mean,
+           SWE = if_else(SWE < 0, 0, SWE)) %>% # can't have negative swe
+    dplyr::select(-c(mean, sd, anomaly)) %>%
+    st_as_stars(dims = c('x','y','time')) %>%
+    st_set_crs(st_crs(target_patterns$eofs)) %>%
+    mutate(SWE = units::set_units(SWE, mm))
 }
+
+# loop through amplitudes, for each time row, multiply those numbers by the eofs, at the end join the results?
