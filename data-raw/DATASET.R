031b39bfd26d3f0fd7526a9f5104b52615f72fef
Nick Gauthier <ngauthier@email.arizona.edu>
2021-05-10 12:13:39 -0700

    use s2 for data generation and don't crop coarse model data by default

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,25 +3,25 @@
 library(raster) # processing raster data
 library(tidyverse) # data manipulation and visualization
 library(stars)
+sf_use_s2(TRUE) # use s2 spherical geometry
 
 # download state boundary files for cropping and plotting
-state_names <- c('arizona', 'new mexico', 'colorado',
-                 'california', 'utah', 'nevada',
-                 'oregon', 'washington', 'idaho',
-                 'wyoming', 'montana')
+states_wus <- rnaturalearth::ne_states(country = 'United States of America', returnclass = 'sf') %>%
+  filter(name %in% c('Arizona', 'New Mexico', 'Colorado',
+                     'California', 'Utah', 'Nevada',
+                     'Oregon', 'Washington', 'Idaho',
+                     'Wyoming', 'Montana'))
 
-states_wus <- maps::map('state', regions = state_names,
-                        fill = TRUE, plot = FALSE) %>%
+world <- maps::map('world', wrap=c(0, 360), fill = TRUE, plot = FALSE) %>%
   st_as_sf()
 
-world <- maps::map('world',wrap=c(0,360),fill = TRUE, plot = FALSE) %>%
-  st_as_sf()
+#rnaturalearth::ne_countries(returnclass = 'sf') %>% st_wrap_dateline() %>% plot
 
 
 ## Geographic Data
 
 #Define a study area to constrain all computations.
-bbox <- extent(c(-125, -102, 31, 49))
+bbox <- extent(st_bbox(states_wus))# extent(c(-125, -102, 31, 49))
 
 
 preprocess <- function(x, var, bbox, flip = FALSE, regrid = FALSE, daily = FALSE) {
@@ -33,7 +33,7 @@ preprocess <- function(x, var, bbox, flip = FALSE, regrid = FALSE, daily = FALSE
 
   raster::subset(maps, indices) %>%
     {if(flip) rotate(.) else .} %>%
-    crop(bbox) %>%
+    crop(bbox, snap = 'out') %>%
     {if(daily) mean(.) else .} %>% # if a file is one year of daily values
     {if(regrid) aggregate(., fact = 2) else .} # resample to lower res to speed up analysis
 }
@@ -42,21 +42,21 @@ prism <- list.files('data-raw/UA-SWE', pattern = 'SWE_Depth', full.names = TRUE)
   map(preprocess, var = 'SWE', bbox = bbox, regrid = TRUE, daily = TRUE) %>%
   brick() %>%
   mask(., all(near(., 0)), maskvalue = 1) %>% # mask pixels that never receive snow
-  mask(states_wus) %>% # crop to state boundaries
   st_as_stars() %>%
   setNames('SWE') %>%
   st_set_dimensions('band', values = 1982:2017, names = 'time') %>%
-  mutate(SWE = units::set_units(SWE, mm))
+  mutate(SWE = units::set_units(SWE, mm)) %>%
+  st_set_crs(4326) %>% # the documentation for UA SWE says its EPSG 4326, but the file says EPSG 4269 -- not a huge difference at this scale so just transform without reprojecting
+  st_crop(states_wus)
 
 ## cera
 cera <- map(1:10, ~ (brick('data-raw/CERA-20C_snow.nc', varname = 'sd', level = .))) %>%
   # this averages over the full ensemble
   reduce(`+`) %>%
   `/`(10) %>%
-  crop(bbox) %>%
-  mask(., all(near(., 0)), maskvalue = 1) %>%
-  mask(states_wus) %>%
+  crop(bbox, snap = 'out') %>%
   `*`(1000) %>% # convert to mm
+  mask(., all(near(., 0)), maskvalue = 1) %>%
   st_as_stars() %>%
   setNames('SWE') %>%
   st_set_dimensions('band', values = 1901:2010, names = 'time') %>%
@@ -67,9 +67,9 @@ land_frac <- read_ncdf('data-raw/_grib2netcdf-webmars-public-svc-blue-003-6fe5ca
                        var = 'lsm') %>%
   adrop(4) %>%
   slice('number', 1) %>%
-  .[st_bbox(cera)]
+  st_crop(st_bbox(cera))
 
-cera <- cera * land_frac
+#cera <- cera * land_frac
 
 # cesm
 cesm_h2osno <- preprocess('data-raw/b.e11.BLMTRC5CN.f19_g16.001.clm2.h0.H2OSNO.085001-184912.nc',
@@ -84,16 +84,11 @@ cesm_h2osno_ext <- preprocess('data-raw/b.e11.BLMTRC5CN.f19_g16.001.clm2.h0.H2OS
 
 cesm <- c(cesm_h2osno, cesm_h2osno_ext) %>%
   brick() %>%
-  #raster::resample(as(cera, 'Raster')) %>%
- # mask(mean(as(cera, 'Raster'))) %>%
+  mask(., all(near(., 0)), maskvalue = 1) %>%
   st_as_stars() %>%
   setNames('SWE') %>%
-  st_crop(states_wus) %>%
   st_set_dimensions('band', values = 850:2005, names = 'time') %>%
-  mutate(SWE = units::set_units(if_else(SWE < 0, 0, SWE), mm)) # fix edge effects from resampling
-
-# fix edge effects from resampling
-#cesm[cesm < 0] <- 0
+  mutate(SWE = units::set_units(SWE, mm))
 
 ccsm_lm <- preprocess('data-raw/snw_LImon_CCSM4_past1000_r1i1p1_085001-185012.nc',
                    var = 'snw',
@@ -107,18 +102,11 @@ ccsm_ext <- preprocess('data-raw/snw_LImon_CCSM4_historical_r1i2p1_185001-200512
 
 ccsm <- c(ccsm_lm, ccsm_ext) %>%
   brick() %>%
-  #raster::resample(as(cera, 'Raster')) %>%
-  #mask(mean(as(cera, 'Raster'))) %>%
+  mask(., all(near(., 0)), maskvalue = 1) %>%
   st_as_stars() %>%
   setNames('SWE') %>%
-  st_crop(states_wus) %>%
   st_set_dimensions('band', values = 850:2005, names = 'time') %>%
-  mutate(SWE = units::set_units(if_else(SWE < 0, 0, SWE), mm)) # fix edge effects from resampling
-
-# fix edge effects from resampling
-#ccsm[ccsm < 0] <- 0
-
-#plot(mean(prism));plot(mean(cera));plot(mean(cesm));plot(mean(ccsm))
+  mutate(SWE = units::set_units(SWE, mm))
 
 ###### climate data for teleconnection analysis
 
@@ -221,34 +209,7 @@ geop_ond <- brick('data-raw/adaptor.mars.internal-1584737536.594777-6445-11-8d0f
 #  unnest(rasters) %>%
 #  dplyr::select(-X1982)
 
-#Turn the raster bricks into data frames and join.
-# prism_dat <- prism %>%
-#   as.data.frame(xy = TRUE, na.rm = TRUE, long = TRUE) %>%
-#   mutate(year = round(parse_number(layer))) %>%
-#   rename(SWE = value) %>%
-#   dplyr::select(-layer)
-#
-# cera_dat <- cera %>%
-#   as.data.frame(xy = TRUE, na.rm = TRUE, long = TRUE) %>%
-#   mutate(year = round(parse_number(layer))) %>%
-#   rename(SWE = value) %>%
-#   dplyr::select(-layer)
-#
-# cesm_dat <- cesm %>%
-#   as.data.frame(xy = TRUE, na.rm = TRUE, long = TRUE) %>%
-#   mutate(year = str_sub(layer, 2) %>% parse_number() %>% round) %>%
-#   rename(SWE = value) %>%
-#   select(-layer)
-#
-# ccsm_dat <- ccsm %>%
-#   as.data.frame(xy = TRUE, na.rm = TRUE, long = TRUE) %>%
-#   mutate(year = str_sub(layer, 2) %>% parse_number() %>% round) %>%
-#   rename(SWE = value) %>%
-#   select(-layer)
-
-#usethis::use_data(prism_dat, cera_dat, cesm_dat, ccsm_dat, tmean_dat, ppt_dat, geop_jfm, sst_dat, areas_prism, areas_cera, states_wus, world, internal = TRUE, overwrite = TRUE)
-
-usethis::use_data(prism, cera, cesm, ccsm, prism_clim, geop_jfm, sst, states_wus, world, internal = TRUE, overwrite = TRUE)
+usethis::use_data(prism, cera, cesm, ccsm, prism_clim, geop_jfm, sst, land_frac, states_wus, world, internal = TRUE, overwrite = TRUE)
 
 
 ## noaa
