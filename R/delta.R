5644ba56c27e27f7170a86a26f6c86eb2659a1c9
Nick Gauthier <ngauthier@email.arizona.edu>
2022-06-02 15:26:15 -0400

    better handling of dimensions in delta functions

--- R/delta.R
+++ R/delta.R
@@ -13,24 +13,32 @@ delta_mul <- function(pred, obs, newdata = NULL, monthly = FALSE) {
   pred_clim <- get_climatology(pred, monthly = monthly)
   obs_clim <- get_climatology(obs, monthly = monthly)
 
+  # setup the new dimensions
+  new_dims <- st_dimensions(obs)
+  new_dims$time <- st_dimensions(newdata)$time # use time dimensions from newdata
+
   (newdata / pred_clim) %>%
-    st_warp(slice(obs, 'time', 1), use_gdal = TRUE, method = 'bilinear') %>%
+    st_warp(slice(obs, 'time', 1), use_gdal = TRUE, method = 'bilinear', no_data_value = -99999) %>%
     setNames(names(newdata)) %>%
     mutate(across(everything(), ~units::set_units(.x, units(newdata[[1]]), mode = 'standard'))) %>%
-    st_set_dimensions('band', values = st_get_dimension_values(newdata, 'time'), names = 'time') %>%
+    `st_dimensions<-`(new_dims) %>% # restore original dimensions
     `*`(obs_clim)
 }
 
 #' @export
-delta_add <- function(pred, obs, newdata = NULL, monthly = NULL) {
+delta_add <- function(pred, obs, newdata = NULL, monthly = FALSE) {
   if(is.null(newdata)) newdata <- pred
   pred_clim <- get_climatology(pred, monthly = monthly)
   obs_clim <- get_climatology(obs, monthly = monthly)
 
+  # setup the new dimensions
+  new_dims <- st_dimensions(obs)
+  new_dims$time <- st_dimensions(newdata)$time # use time dimensions from newdata
+
   (newdata - pred_clim) %>%
-    st_warp(slice(obs, 'time', 1), use_gdal = TRUE, method = 'bilinear') %>%
+    st_warp(slice(obs, 'time', 1), use_gdal = TRUE, method = 'bilinear', no_data_value = -99999) %>%
     setNames(names(newdata)) %>%
     mutate(across(everything(), ~units::set_units(.x, units(newdata[[1]]), mode = 'standard'))) %>%
-    st_set_dimensions('band', values = st_get_dimension_values(newdata, 'time'), names = 'time') %>%
+    `st_dimensions<-`(new_dims) %>% # restore original dimensions
     `+`(obs_clim)
 }
