8d6dc22d63be0cc57df942d1986b1d4aed79bc4b
Nick Gauthier <nicolas.gauthier@ufl.edu>
2023-06-19 21:51:02 -0400

    fix lat weight/normalizing order

--- R/get_patterns.R
+++ R/get_patterns.R
@@ -12,8 +12,10 @@
 #'
 get_patterns <- function(dat, k = 4, scale = FALSE, rotate = FALSE, monthly = FALSE, weight = TRUE){
 
+  if(weight) dat <- dat * lat_weights(dat) # weight by sqrt cosine latitude, in radians
+
   climatology <- get_climatology(dat, monthly = monthly)
-  pca <- get_pcs(dat, scale = scale, clim = climatology, monthly = monthly, weight = weight)
+  pca <- get_pcs(dat, scale = scale, clim = climatology, monthly = monthly)
   eigenvalues <- get_eigenvalues(pca)
   eofs <- get_eofs(dat, pca, k, rotate)
 
@@ -46,7 +48,7 @@ get_patterns <- function(dat, k = 4, scale = FALSE, rotate = FALSE, monthly = FA
 
 #' @export
 print.patterns <- function(obj) {
-  print(paste0('A patterns object with k = ', obj$k, ', scale = ', obj$scale,
+  print(paste0('A `pattern` object with k = ', obj$k, ', scale = ', obj$scale,
                ', monthly = ', obj$monthly, ', and rotate = ', obj$rotate))
   print(obj$eofs)
 }
--- R/get_pcs.R
+++ R/get_pcs.R
@@ -9,13 +9,10 @@
 #'
 #' @examples
 
-get_pcs <- function(dat, scale = FALSE, clim = NULL, monthly = FALSE, weight = TRUE) {
-
-  #if(weight) dat <- dat * lat_weights(dat) # weight by sqrt cosine latitude, in radians
+get_pcs <- function(dat, scale = FALSE, clim = NULL, monthly = FALSE) {
 
   dat %>%
     get_anomalies(clim = clim, scale = scale, monthly = monthly) %>%
-    {if(weight) . * lat_weights(.) else .} %>%
     split('time') %>% # split along the time dimension
     setNames(st_get_dimension_values(dat, 'time')) %>%
     as_tibble() %>%
